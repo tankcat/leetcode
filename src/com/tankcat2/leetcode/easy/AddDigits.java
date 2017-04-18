2c2d2612835e833d69f8c040ac8e34740dc267c2
tankcat <15720622962wxt@gmail.com>
2017-04-18 20:06:31 +0800

    code

--- src/com/tankcat2/leetcode/easy/AddDigits.java
+++ src/com/tankcat2/leetcode/easy/AddDigits.java
@@ -1,5 +1,9 @@
 package com.tankcat2.leetcode.easy;
 
+import com.sun.management.OperatingSystemMXBean;
+
+import java.lang.management.ManagementFactory;
+
 /**
  * Created by stream on 17-3-31.
  */
@@ -22,5 +26,14 @@ public class AddDigits {
     public static void main(String[] args){
         AddDigits ad=new AddDigits();
         System.out.println(ad.addDigits(10));
+        OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+        for(int i=0;i<Integer.MAX_VALUE;i++) {
+            double pi = 0;
+            double dx = 1e-3;
+            for (double x = -100; x <= +100; x += dx) {
+                pi += Math.exp(-x * x) * dx;
+            }
+            System.out.println(osBean.getProcessCpuLoad());//指CPU的负载情况
+        }
     }
 }
