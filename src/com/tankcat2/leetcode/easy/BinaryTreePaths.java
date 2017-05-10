2257fc9ee24d6c2378ba1042e8a25550799e9be3
tankcat <15720622962wxt@gmail.com>
2017-05-10 09:28:32 +0800

    code

---
+++ src/com/tankcat2/leetcode/easy/BinaryTreePaths.java
@@ -0,0 +1,46 @@
+package com.tankcat2.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by stream on 17-5-10.
+ * Given a binary tree, return all root-to-leaf paths.
+
+ For example, given the following binary tree:
+
+ 1
+ /   \
+ 2     3
+ \
+ 5
+ All root-to-leaf paths are:
+
+ ["1->2->5", "1->3"]
+ */
+public class BinaryTreePaths {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            val=x;
+        }
+    }
+
+    private List<String> ret=new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        if(root!=null)
+            findPaths(root,String.valueOf(root.val));
+        return ret;
+    }
+
+    private void findPaths(TreeNode root, String path){
+        if(root.left==null&&root.right==null)
+            ret.add(path);
+        if(root.left!=null)
+            findPaths(root.left,path+"->"+root.left.val);
+        if(root.right!=null)
+            findPaths(root.right,path+"->"+root.right.val);
+    }
+}
